9170c34a3de9ca17a3a8bbe71073f1978b1037ab
Flavio <riverolauraflaviocesar07@gmail.com>
2026-01-22 15:40:47 -0500

    nuevo

--- client/src/components/ActivityModals/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as NoteModal } from './NoteModal';
-export { default as CallModal } from './CallModal';
-export { default as TaskModal } from './TaskModal';
-export { default as DealModal } from './DealModal';
-export { default as CompanyModal } from './CompanyModal';
-export { default as ContactModal } from './ContactModal';
